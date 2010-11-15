a210ab1efefc0c64c1b524683f66eb06ca7e4e01
aniket.aries <aniket.aries@0d08d425-acf4-1cf2-8e34-329252263baa>
2010-11-15 22:07:08 +0000

    Restructuring the architecture.

--- trunk/samples/UseThisForTestingFacade_4.java
+++
@@ -1,6 +0,0 @@
-            Scanner scanner = new Scanner(file);
-            ffffff ggggjj 555 yyyyyy hhhh
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                System.out.println(line);
-            }
