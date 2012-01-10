f2d84c336a6a02e2f0285621e175d3a17a32221b
vasanth.kmy@gmail.com <vasanth.kmy@gmail.com@0d08d425-acf4-1cf2-8e34-329252263baa>
2012-01-10 05:49:04 +0000

    Returns scrapeResult object which also has the searchQueryID along with the LinkedHashSet searchResultObj.

--- trunk/itjava/src/itjava/scraper/ScrapeApiInfoMain.java
+++ trunk/itjava/src/itjava/scraper/ScrapeApiInfoMain.java
@@ -5,6 +5,6 @@ import java.util.LinkedHashSet;
 public class ScrapeApiInfoMain {
 	public static void main(String[] args) {
 		String query = "Reading a file using Java";
-		LinkedHashSet <ScrapeData> scrapeFinalObj = InfoScrape.ScrapeSites(query);
+		ScrapeResult scrapeFinalObj = InfoScrape.ScrapeSites(query);
 	}
 }
