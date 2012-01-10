70ae21879d4b8891f0417fc7e6d289a2b47f887e
vasanth.kmy@gmail.com <vasanth.kmy@gmail.com@0d08d425-acf4-1cf2-8e34-329252263baa>
2012-01-10 08:59:31 +0000

    Changed the return type of scrapeFinalObj to LinkedHashSet<ScrapeData>

--- trunk/itjava/src/itjava/scraper/ScrapeApiInfoMain.java
+++ trunk/itjava/src/itjava/scraper/ScrapeApiInfoMain.java
@@ -5,6 +5,6 @@ import java.util.LinkedHashSet;
 public class ScrapeApiInfoMain {
 	public static void main(String[] args) {
 		String query = "Reading a file using Java";
-		ScrapeResult scrapeFinalObj = InfoScrape.ScrapeSites(query);
+		LinkedHashSet<ScrapeData> scrapeFinalObj = InfoScrape.ScrapeSites(query);
 	}
 }
