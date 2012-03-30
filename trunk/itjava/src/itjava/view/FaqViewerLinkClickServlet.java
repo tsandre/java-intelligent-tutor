2899a254b61b1ae71ba813572814520a5a5a3c08
bharatwaja@gmail.com <bharatwaja@gmail.com@0d08d425-acf4-1cf2-8e34-329252263baa>
2012-03-30 05:41:26 +0000

    FAQ Viewer (old)

---
+++ trunk/itjava/src/itjava/view/FaqViewerLinkClickServlet.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package itjava.view;
+
+import itjava.scraper.DBConnection;
+import itjava.scraper.ScrapeData;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.eclipse.jdt.core.dom.EnumDeclaration;
+
+/**
+ * @author Bharat
+ * 
+ */
+@WebServlet("/FaqViewerLinkClickServlet")
+public class FaqViewerLinkClickServlet extends HttpServlet {
+
+	private Connection _conn;
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("In servlet");
+		System.out.println(request.getParameter("scrapeId"));
+		HttpSession session = request.getSession(true);
+		String userId = (String) session.getAttribute("userName");
+		System.out.println("userid: "+userId);
+		
+
+		try {
+
+			_conn = DBConnection.GetConnection();
+
+			String insertQuery = "insert into FAQViewerLinkLog"
+					+ "(user, scrapeId, timeAccessed)"
+					+ " values (?,?,datetime('now'));";
+			PreparedStatement addFaqSelection = _conn
+					.prepareStatement(insertQuery);
+			addFaqSelection.setInt(2,
+					Integer.valueOf(request.getParameter("scrapeId")));
+			
+			//TODO: user Id should go here. 
+			addFaqSelection.setString(1, "UserNameHere");
+			addFaqSelection.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			try {
+				response.getWriter().println(
+						"Values inserted in DB successfully.");
+				_conn.close();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
