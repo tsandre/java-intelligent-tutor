74f8170a630cad3ef422916296eb21cc040c5f77
vasanth.kmy@gmail.com <vasanth.kmy@gmail.com@0d08d425-acf4-1cf2-8e34-329252263baa>
2012-04-23 18:26:36 +0000

    To test Google Custom Search API

---
+++ trunk/itjava/src/itjava/scraper/testGoogleCustomSearch.java
@@ -0,0 +1,63 @@
+package itjava.scraper;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.customsearch.Customsearch;
+import com.google.api.services.customsearch.model.Result;
+import com.google.api.services.customsearch.model.Search;
+import org.jsoup.*;
+
+/**
+ * @author Vasanth Krishnamoorthy
+ * 
+ */
+public class testGoogleCustomSearch{
+	public static void main(String args[]){
+		GoogleSearch("Connecting to Database");
+	}
+	private static ArrayList<String> GoogleSearch(String query) {
+		ArrayList<String> googleSearchResults = new ArrayList<String>();
+		String googleSearchContext = "011045704107476092050:liivikkqg0k";
+		String googleSearchAPIKey = "AIzaSyCdECK7u3Erfb23uPqCEl2lESCDw4cwOXA";
+		try {
+			NetHttpTransport nht = new NetHttpTransport();
+			JacksonFactory jf = new JacksonFactory();
+			Customsearch customsearch = new Customsearch(nht, jf);
+			Customsearch.Cse.List request = customsearch.cse().list(query+" site:java2s.com");
+			request.setKey(googleSearchAPIKey);
+			request.setCx(googleSearchContext);
+
+			for(long iResultIndex=1; iResultIndex<50;iResultIndex+=10) {
+				request.setStart(iResultIndex);
+				Search results = request.execute();
+				List<Result> searchResults = results.getItems();
+				for(Result item:searchResults) {
+					String urlText = item.getLink();
+					String urlEnding = urlText.toUpperCase();
+					System.out.println("GoogleResult: "+urlText);
+					urlText = Jsoup.parse(urlText).text();
+					String title = item.getTitle();
+					title = Jsoup.parse(title).text();
+					//		System.out.println("Title: "+title);
+					String summary = item.getSnippet().trim();
+					summary = Jsoup.parse(summary).text();
+					//		System.out.println("Summary: "+summary);
+					if (!urlEnding.endsWith(".PDF") && !urlEnding.endsWith(".DOC") && !urlEnding.endsWith(".PPT")) {
+						googleSearchResults.add(urlText);
+					}
+				}
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return googleSearchResults;
+	}
+
+}
+
